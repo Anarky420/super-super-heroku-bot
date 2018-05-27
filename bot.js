68f0db8277bd4d98ad00752ec0a5195f67b53f10
Anarky420 <39548876+Anarky420@users.noreply.github.com>
2018-05-27 20:55:54 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on("message", (message) => {
+  if(message.content === 'Dunban Talent Art') {
+    message.channel.send('Born In A World Of Strife, Against The Odds, We Choose To Fight, Blossom Dance!.');
+  }
+  if(message.content === 'Shulk Talent Art') {
+    message.channel.send('Monado Lend Us Your Power');
+  }
+  if(message.content === 'Melia Talent Art') {
+    message.channel.send('Manifest Yourself');
+  }
+  if(message.content === 'Sharla Talent Art') {
+    message.channel.send('Gotta Cool Off);
+  }
+  if(message.content === 'Fiora Talent Art') {
+    message.channel.send('Sword Drones!');
+  }
+if(message.content === 'Riki Talent Art') {
+    message.channel.send('Riki Find Goodies');
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
